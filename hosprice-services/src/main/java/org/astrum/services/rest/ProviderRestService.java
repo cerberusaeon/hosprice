192e115e8c44083df88b18e7a68d4d8b957ea29d
Stephen Ohimor <sohimor@dokkit.com>
2013-05-16 01:08:18 -0400

    backbone js addition

--- hosprice-services/src/main/java/org/astrum/services/rest/ProviderRestService.java
+++ hosprice-services/src/main/java/org/astrum/services/rest/ProviderRestService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
--- hosprice-web/src/main/webapp/js/app.js
+++ hosprice-web/src/main/webapp/js/app.js
@@ -1,32 +1,116 @@
 var Hosprice = Hosprice || {};
 
 
+Provider = Backbone.Model.extend({
+	url : function(){
+		return "/hosprice/rest/provider/zipcode/"+this.get("zipcode");
+	},
+	
+	defaults: {
+		providerId: 0,
+		legacyId: '',
+		name: 'Temp Result',
+		address1: '',
+		address2: '',
+		city: '',
+		state: '',
+		zipcode: '',
+		regionName: '',
+		regionState: '',
+		diagnosisRelatedGroupModels: {}
+	},
+	  validate: function( attributes ){
+		  console.log("here we go");
+          if( attributes.name === 'blue' ){
+        	  console.log("Get in my belly");
+              return "You can't be blue";
+          }
+      },
+    initialize: function(){
+        alert("Welcome to this world");
+        this.on("change:name", function(model){
+            var name = model.get("name"); 
+            alert("Changed my name to " + name );
+        });
+        
+        this.bind("invalid", function(model, error){
+            // We have received an error, log it, alert it or forget it :)
+            alert( error );
+        });
+    },
+    booberies: function(newName){
+    	this.set({name: newName});
+    	console.log("I thought it was a costume ball");
+    }
+   
+});
+
+
+
+/*var provider2 = new Provider;
+provider2.booberies('red');
+provider2.set({name: 'green'}, {'validate': 'true'});
+provider2.set({name: 'blue'}, {'validate': 'true'});*/
+
 $(document).ready(function(){
 	console.log("executed...");
-	$('#contact-form').on("submit", function(event){
+		
+	
+	$('#provider-form').on("submit", function(event){
 		console.log("clicked form...");
 		event.preventDefault();
 		
+		var zipcode = $('#provider-form input[name=zipcode]').val();
+		var state = $('#provider-form input[name=state]').val();
+		var city = $('#provider-form input[name=city]').val();
+		var provider = new Provider({zipcode:zipcode, state:state, city:city});
 		
-		var contactInformation = {};
-		contactInformation.name = $('#contact-form input[name=name]').val();
-		contactInformation.email = $('#contact-form input[name=contactEmail]').val();
-		contactInformation.companyName = $('#contact-form input[name=companyName]').val(); 
-		contactInformation.website = $('#contact-form input[name=website]').val();
-		contactInformation.reason = $('#contact-form textArea[name=reason]').val();
-		console.log(contactInformation);
-		contactInformation = JSON.stringify(contactInformation);
+		provider.fetch({
+	        success: function (provider) {
+	        	console.log("success fetching provider via backbone...");
+	        	console.log(provider);
+	            alert(provider.toJSON());
+	        },
+	        error: function(xhr, textStatus, error){
+	        	console.log("did we get an error");
+				  console.log(xhr.statusText);
+				  console.log(textStatus);
+				  console.log(error);
+	        }
+	    });
+		
 
-		var url = "/hosprice/rest/user/contact";
+		
+	/*	var providerModel = {};
+		providerModel.zipcode = $('#provider-form input[name=zipcode]').val();
+		providerModel.state = $('#provider-form input[name=state]').val();
+		providerModel.city = $('#provider-form input[name=city]').val(); 
+		
+		console.log(providerModel);
+		providerModel = JSON.stringify(providerModel);
+
+		var url = "/hosprice/rest/provider/zipcode/"+providerModel.zipcode;
 		$.ajax(url, {
 			crossDomain: true,
 			contentType: "application/json",
-			data: contactInformation,
+			data: providerModel,
 			type: "POST",
 			success: function(data) {
 				console.log("data was saved correctly");
-				$('#launchThanksModal').click();
-				//var myData = $.parseJSON(data);
+				//var i, recommendations = $("[id^=result-]");
+				//for(i = 0; i < recommendations.length; i++){
+				//	$(recommendations[i]).fadeOut(2000).delay(1000).fadeIn(2000);
+					
+				//}
+				
+				console.log(modelData);
+				$("[id^=result-]").each(function(){
+						$(this).fadeOut(2000).delay(1000);
+						renderNew
+						console.log("id is: "+$(this).attr("id"));
+						$(this).delay(3000);
+				});
+				
 			},
 			error: function(xhr, textStatus, error){
 							  console.log(xhr.statusText);
@@ -34,7 +118,7 @@ $(document).ready(function(){
 							  console.log(error);
 			}
 		});
-		
+	*/	
 		return false;
 	});
 	
