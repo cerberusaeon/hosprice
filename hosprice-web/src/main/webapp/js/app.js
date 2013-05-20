d43d5de40a3172f6caa83efa98baf7c87b927030
Stephen Ohimor <sohimor@dokkit.com>
2013-05-20 08:36:50 -0400

    creating JS model

--- hosprice-web/src/main/webapp/js/app.js
+++ hosprice-web/src/main/webapp/js/app.js
@@ -1,5 +1,31 @@
-var Hosprice = Hosprice || {};
+var DataMap = function(options){
+	var searchInputHospital = options.searchInputHospital;
+	var searchInputDiagnosis = options.searchInputDiagnosis;
+	var searchInputZipcode = options.searchInputZipcode;
+	var searchInputState = options.searchInputState;
+	var searchInputCity = options.searchInputCity;
+	
+	this.getSearchInputCity = function(){
+		return searchInputCity;
+	}
+	
+	this.sayHello = function(){
+		console.log("why hello?");
+	}
+}
 
+var Hosprice = function(options){
+	this.dataMap = new DataMap({searchInputCity:'fartCity'});
+	
+	this.hospriceHello = function(){
+		console.log("hosprice hello!");
+	}
+}
+
+var hosprice = new Hosprice();
+hosprice.dataMap.sayHello();
+hosprice.hospriceHello();  
+console.log(hosprice.dataMap.getSearchInputCity());  //using dynamically created getter
 
 Provider = Backbone.Model.extend({
 	url : function(){
@@ -45,31 +71,72 @@ Provider = Backbone.Model.extend({
    
 });
 
+var ProviderCollection = Backbone.Collection.extend({
+	  model: Provider,
+	  zipcode: '36302',
+	  url : function(){
+			return "/hosprice/rest/provider/zipcode/"+this.zipcode;
+		},
+		initialize: function(){
+			console.log("initializing the collection...");
+			console.log(this.zipcode);
+		}
+});
 
 
-/*var provider2 = new Provider;
-provider2.booberies('red');
-provider2.set({name: 'green'}, {'validate': 'true'});
-provider2.set({name: 'blue'}, {'validate': 'true'});*/
+ProviderView = Backbone.View.extend({
+    tagName: "div",
+    id: "results-container",
+    template: _.template($("#template-recommendation").html()),
+    initialize: function() {
+		    this.model.bind("change", this.render, this);
+		    this.render();
+		  },
+
+		  render: function() {
+			  console.log("DId you fucking render");
+			  console.log(this.model.attributes);
+			  //this.$el.html(this.template(this.model.attributes));
+			  $("#results-container").append(this.$el.html(this.template(this.model.attributes)));
+			  return this;
+		  }
+});
+
+
+
+/*
+ * var provider2 = new Provider; provider2.booberies('red');
+ * provider2.set({name: 'green'}, {'validate': 'true'}); provider2.set({name:
+ * 'blue'}, {'validate': 'true'});
+ */
 
 $(document).ready(function(){
 	console.log("executed...");
-		
+	
+	//hosprice.dataMap.test();
 	
 	$('#provider-form').on("submit", function(event){
 		console.log("clicked form...");
 		event.preventDefault();
-		
-		var zipcode = $('#provider-form input[name=zipcode]').val();
+		//hosprice.dataMap.test();
+		var zipcode2 = $('#provider-form input[name=zipcode]').val();
 		var state = $('#provider-form input[name=state]').val();
 		var city = $('#provider-form input[name=city]').val();
-		var provider = new Provider({zipcode:zipcode, state:state, city:city});
 		
-		provider.fetch({
-	        success: function (provider) {
+		var providerCollection = new ProviderCollection({zipcode:zipcode2});
+		console.log("===== provider =====");
+		console.log(providerCollection);
+		providerCollection.zipcode = zipcode2;
+		console.log(providerCollection.zipcode);
+		console.log("===== provider =====");
+		//var provider = new Provider({zipcode:zipcode, state:state, city:city});
+		//var providerView = new ProviderView({model: provider});
+		providerCollection.fetch({
+	        success: function (result, options) {
 	        	console.log("success fetching provider via backbone...");
-	        	console.log(provider);
-	            alert(provider.toJSON());
+	        	var myProvider = providerCollection.at(0);
+	        	console.log(providerCollection);
+	        	
 	        },
 	        error: function(xhr, textStatus, error){
 	        	console.log("did we get an error");
@@ -79,46 +146,31 @@ $(document).ready(function(){
 	        }
 	    });
 		
-
 		
-	/*	var providerModel = {};
-		providerModel.zipcode = $('#provider-form input[name=zipcode]').val();
-		providerModel.state = $('#provider-form input[name=state]').val();
-		providerModel.city = $('#provider-form input[name=city]').val(); 
-		
-		console.log(providerModel);
-		providerModel = JSON.stringify(providerModel);
-
-		var url = "/hosprice/rest/provider/zipcode/"+providerModel.zipcode;
-		$.ajax(url, {
-			crossDomain: true,
-			contentType: "application/json",
-			data: providerModel,
-			type: "POST",
-			success: function(data) {
-				console.log("data was saved correctly");
-				//var i, recommendations = $("[id^=result-]");
-				//for(i = 0; i < recommendations.length; i++){
-				//	$(recommendations[i]).fadeOut(2000).delay(1000).fadeIn(2000);
-					
-				//}
-				
-				console.log(modelData);
-				$("[id^=result-]").each(function(){
-						$(this).fadeOut(2000).delay(1000);
-						renderNew
-						console.log("id is: "+$(this).attr("id"));
-						$(this).delay(3000);
-				});
-				
-			},
-			error: function(xhr, textStatus, error){
-							  console.log(xhr.statusText);
-							  console.log(textStatus);
-							  console.log(error);
-			}
-		});
-	*/	
+	/*
+	 * var providerModel = {}; providerModel.zipcode = $('#provider-form
+	 * input[name=zipcode]').val(); providerModel.state = $('#provider-form
+	 * input[name=state]').val(); providerModel.city = $('#provider-form
+	 * input[name=city]').val();
+	 * 
+	 * console.log(providerModel); providerModel =
+	 * JSON.stringify(providerModel);
+	 * 
+	 * var url = "/hosprice/rest/provider/zipcode/"+providerModel.zipcode;
+	 * $.ajax(url, { crossDomain: true, contentType: "application/json", data:
+	 * providerModel, type: "POST", success: function(data) { console.log("data
+	 * was saved correctly"); //var i, recommendations = $("[id^=result-]");
+	 * //for(i = 0; i < recommendations.length; i++){ //
+	 * $(recommendations[i]).fadeOut(2000).delay(1000).fadeIn(2000);
+	 * 
+	 * //}
+	 * 
+	 * console.log(modelData); $("[id^=result-]").each(function(){
+	 * $(this).fadeOut(2000).delay(1000); renderNew console.log("id is:
+	 * "+$(this).attr("id")); $(this).delay(3000); }); }, error: function(xhr,
+	 * textStatus, error){ console.log(xhr.statusText); console.log(textStatus);
+	 * console.log(error); } });
+	 */	
 		return false;
 	});
 	
